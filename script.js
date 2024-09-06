f20efce002446078ee696dafa3e0d0f54fbc72a1
tobia1k <113366743+tobia1k@users.noreply.github.com>
2024-09-06 16:01:15 -0400

    Add new grid button, random colors, and darkening
    The page will load with a 16x16 grid by default. If the user
    clicks the "New Grid" button, the user will be prompted to
    input how many squares on each side they want the new grid
    to have. The user cannot enter a number greater than 100. The
    current grid is deleted and a new one with the corresponding size
    is created in its place.
    
    As the user hovers the cursor over each square, the squares will
    be colored in with a random color and will progressively get 10%
    darker each time the user's cursor hovers over that square.

--- script.js
+++ script.js
@@ -1,20 +1,62 @@
-
-let gridSize = 16;
 let container = document.querySelector("#container");
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-    let square = document.createElement("div");
-    square.setAttribute("id", i);
-    square.setAttribute("class", "square");
-    square.style.height = (100 / gridSize) + "%";
-    square.style.width = (100 / gridSize) + "%";
-    container.appendChild(square);
+let button = document.createElement("button");
+document.body.insertBefore(button, container);
+button.textContent = "New Grid";
+button.addEventListener('click', () => {
+    let newSize = null;
+    do {
+        newSize = prompt("How many squares do you want on each side?");
+    } while (newSize > 100 || !newSize);
+    deleteGrid();
+    createGrid(newSize);
+})
+
+createGrid(16);
+
+function createGrid(size = 16) {
+    for (let i = 0; i < size * size; i++) {
+        let square = document.createElement("div");
+        square.setAttribute("id", i);
+        square.setAttribute("class", "square");
+        square.style.height = (100 / size) + "%";
+        square.style.width = (100 / size) + "%";
+        container.appendChild(square);
+    }
+    draw();
 }
 
-document.addEventListener('mouseenter', event => {
+function deleteGrid() {
+    let child = container.lastElementChild;
+    while (child) {
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+}
 
-})
+function draw() {
+    const squares = document.querySelectorAll(".square");
 
-document.addEventListener('mouseleave', event => {
-    
-})
+    for (const elem of squares) {
+        let opacityOfElem = 0;
+        elem.addEventListener('mouseenter', () => {
+
+            elem.classList.add("hovered");
+            elem.style.backgroundColor = getRandomColor();
+
+            if (opacityOfElem <= 1) {
+                opacityOfElem += 0.1;
+                elem.style.opacity = opacityOfElem;
+            }
+        });
+    }
+}
+
+function getRandomColor() {
+    let letter = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letter[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
